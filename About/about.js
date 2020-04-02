2aef79aaf3d9b32105536d7f4e26866e9aa0012e
Spencer Kenealy <spencerkenealy@Spencers-iMac.local>
2020-04-02 09:31:06 -0700

    initial

---
+++ About/about.js
@@ -0,0 +1,9 @@
+const burgerMenu = () => {
+    let x = document.querySelector("#myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
+
---
+++ Contact/contact.js
@@ -0,0 +1,8 @@
+const burgerMenu = () => {
+  let x = document.querySelector("#myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
